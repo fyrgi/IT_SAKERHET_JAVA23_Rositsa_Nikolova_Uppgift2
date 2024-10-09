d6a5948f73294e701206460e868e9294b01871e3
Rositsa Nikolova <rositsa.se@gmail.com>
2024-10-09 21:57:27 +0200

    Initial commit

---
+++ timelord/src/main/java/com/tasktwo/timelord/TimelordApplication.java
@@ -0,0 +1,13 @@
+package com.tasktwo.timelord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelordApplication.class, args);
+	}
+
+}
---
+++ timelord/src/test/java/com/tasktwo/timelord/TimelordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktwo.timelord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
